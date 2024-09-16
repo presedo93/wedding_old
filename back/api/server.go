c18d418d98c617e62656bfb5d34cce3a7f6661c5
Rene Presedo <presedo93@gmail.com>
2024-09-16 20:21:13 +0200

    feat: basic users API using Gin

---
+++ back/api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/presedo93/wedding/back/db/sqlc"
+)
+
+type Server struct {
+	store  db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing.
+func NewServer(store db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.GET("/users/:id", server.getUser)
+	router.GET("/users", server.listUser)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address.
+func (s *Server) Start(address string) error {
+	return s.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- back/main.go
+++ back/main.go
@@ -1,13 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/presedo93/wedding/back/api"
+	db "github.com/presedo93/wedding/back/db/sqlc"
+)
+
+const (
+	dbSource      = "postgresql://rendres:s3cr3t@localhost:5432/wedding?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
