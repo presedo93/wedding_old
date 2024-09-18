677bbc61060d0ff282be87c2cfe65b66aadab6c3
Rene Presedo <presedo93@gmail.com>
2024-09-18 21:25:39 +0200

    fix: test to read env vars

--- back/util/config_test.go
+++ back/util/config_test.go
@@ -1,32 +1,17 @@
 package util
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestReadEnvFile(t *testing.T) {
-	tmp, err := os.CreateTemp(".", ".env")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Clean up
-	defer os.Remove(tmp.Name())
-
-	// Write some test data to it
-	text := []byte("DATABASE_URL=test_db_url\nSERVER_ADDRESS=test_server_address")
-	if _, err := tmp.Write(text); err != nil {
-		t.Fatal(err)
-	}
-
-	conf, err := LoadEnv(".", tmp.Name())
+func TestReadEnvVars(t *testing.T) {
+	conf, err := LoadEnv("../", ".env")
 	if err != nil {
 		t.Fatal("Error loading .env file")
 	}
 
-	assert.Equal(t, "test_db_url", conf.DatabaseURL)
-	assert.Equal(t, "test_server_address", conf.ServerAddress)
+	assert.Equal(t, "postgresql://rendres:s3cr3t@localhost:5432/wedding?sslmode=disable", conf.DatabaseURL)
+	assert.Equal(t, "0.0.0.0:8080", conf.ServerAddress)
 }
