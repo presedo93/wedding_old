ca354dbb12b473e3f84b1b54d5b7802d8d42dbce
Rene Presedo <presedo93@gmail.com>
2024-09-19 18:31:38 +0200

    feat(back): include SuperTokens in the compose file and viper to load env vars (#4)
    * feat: include SuperTokens in the compose file and viper to load env vars
    
    * fix: set enviroment variables in tests workflow
    
    * fix: test to read env vars
    
    * fixup! fix: test to read env vars

---
+++ back/util/config.go
@@ -0,0 +1,27 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config stores the configuration params for the application
+// The values are read by viper from a config fiel or environment variables.
+type Config struct {
+	DatabaseURL   string `mapstructure:"DATABASE_URL"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadEnv reads the configuration from a config file or environment variables.
+func LoadEnv(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
---
+++ back/util/config_test.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadEnvFile(t *testing.T) {
+	conf, err := LoadEnv("../")
+	if err != nil {
+		t.Fatal("Error loading .env file")
+	}
+
+	assert.Equal(t, "postgresql://rendres:s3cr3t@localhost:5432/wedding?sslmode=disable", conf.DatabaseURL)
+	assert.Equal(t, "0.0.0.0:8080", conf.ServerAddress)
+}
